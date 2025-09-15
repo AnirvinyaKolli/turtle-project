29fea63502f669ee320cc6e1c047bb3c3fcd8d8c
AnirvinyaKolli <anirvinya.kolli.01@gmail.com>
2025-09-15 19:40:41 -0400

    more webs!

--- turtle_proj.py
+++ turtle_proj.py
@@ -6,14 +6,26 @@ My turtle project
 from walker import Walker
 from web import Web
 import turtle
-import random
+import time
 
 background = turtle.Screen()
 background.bgcolor("black")
 background.title("Turtle Project")
 background.screensize(400, 400)
 
-web = Web(web_type="spiral", num_walkers=20)
+web = Web(web_type="random", num_walkers=20)
 web.connect()
+time.sleep(3)
+background.clear()
+background.bgcolor("black")
+
+spiral_web = Web(web_type="spiral", num_walkers=20) 
+spiral_web.connect()
+time.sleep(3)
+background.clear()
+background.bgcolor("black")
+
+polygon_web = Web(web_type="polygon", num_walkers=9)
+polygon_web.connect()
 
 turtle.done()
